e123b49cb931094eeb356750f57f7a21efb16729
floflo452 <57909492+floflo452@users.noreply.github.com>
2022-11-14 14:46:09 +0100

    Ajout crud stock

---
+++ services/sql/Stock.crud.php
@@ -0,0 +1,46 @@
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=projetwebl2","root","root");
+
+// ======================= Create Stock =======================
+function create($db) {
+    $stm = $db->prepare("INSERT INTO `stock`(`qty_stock`) VALUES (:qty_stock)");
+
+    $stm->bindValue(":qty_stock", $_GET["qty_stock"]);
+
+    $stm->execute();
+    echo json_encode($stm->fetchAll());
+}
+// ======================= Read Stock Data =======================
+function read($db) {
+    $stm = $db->prepare("SELECT * FROM `stock` WHERE id = :id");
+    $stm->bindValue(":id", $_GET["id"]);
+    $stm->execute();
+    echo json_encode($stm->fetchAll());
+}
+// ======================= Update Stock =======================
+function update($db) {
+    $stm = $db->prepare("UPDATE `stock` SET `qty_stock`=:qty_stock WHERE id =:id");
+
+    $stm->bindValue(":id", $_GET["id"]);
+    $stm->bindValue(":qty_stock", $_GET["qty_stock"]);
+
+    $stm->execute();
+    echo json_encode($stm->fetchAll());
+}
+
+// ======================= Delete Stock =======================
+function delete($db) {
+    $stm = $db->prepare("DELETE FROM `stock` WHERE id = :id");
+    $stm->bindValue(":id", $_GET["id"]);
+    $stm->execute();
+    echo json_encode($stm->fetchAll());
+}
+
+switch($_GET["function"]) {
+    case 'create': create($db); break;
+    case 'read': read($db); break;
+    case 'update': update($db); break;
+    case 'delete': delete($db); break;
+    default: echo "Not found!"; break;
+}
