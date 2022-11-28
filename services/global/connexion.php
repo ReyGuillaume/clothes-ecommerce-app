c162d81ed4931eaf2a1264cd270d1c2670866e1c
NLKzz <nlk.dev.contact@gmail.com>
2022-11-28 10:47:47 +0100

    root/root => user/user

--- services/global/connexion.php
+++ services/global/connexion.php
@@ -1,7 +1,7 @@
 <?php
 
 $dsn = "mysql:host=localhost;dbname=projetwebl2;charset=UTF8";
-$user = 'nico';
-$password = '131169';
+$user = 'user';
+$password = 'user';
 
 $db = new PDO($dsn, $user, $password);
