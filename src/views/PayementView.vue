42a41b6f0298a05606af55b79fba46f456c543ca
PixelGirl20 <105322502+PixelGirl20@users.noreply.github.com>
2022-12-10 15:28:26 +0100

    update fomulaire payement.V2

--- src/views/PayementView.vue
+++ src/views/PayementView.vue
@@ -2,12 +2,15 @@
 <div id="box">
     
     <form method="POST" action="href">
-        <h3>Formulaire de Payement</h3>
-        <label><input type="text" name="Name" placeholder="Name" /></label>
-        <label><input type="email" name="mail" placeholder="Email" /></label>
-        <label><input type="text" name="credit-card" placeholder="Number_card"/></label>
-        <label><input type="date" name="date" /></label>
-        <label><input type="text" name="ccv" placeholder="CCV"/></label>
+        <h3>Payement</h3>
+        <label><input type="text" name="Name" placeholder="Name" required/></label>
+        <label><input type="email" name="mail" placeholder="Email" required/></label>
+        <label><input type="text" name="credit-card" placeholder="Credit cart number" required/></label>
+        <div id="date">
+            <label><input type="number" name="date" min="01" max ="12" required  placeholder="MM"/></label>
+            <label><input type="number" name="date" min="2022" max ="2100" required placeholder="YYYY" /></label>
+        </div>
+        <label><input type="text" name="ccv" placeholder="CCV" required/></label>
         <label><input type="submit" name="payer" value="Payer" id="bouton"/></label>
     </form>
 </div>
@@ -25,10 +28,9 @@ form{
     align-items: center;
     justify-content: center;
     border: solid 1px black;
-    background-color: rgb(248, 247, 238);
-    box-shadow:  -5px -5px 10px #cecece,
-             5px 5px 10px #f2f2f2;
-    border-radius: 25px;
+    background-color: rgb(236, 236, 234);
+    box-shadow:-7px -7px 26px #f2f2f2;
+    border-radius: 8px;
     padding: 1em;
     width: auto;
     height: auto;
@@ -40,11 +42,20 @@ input{
 
 #bouton{
     padding: 3px;
-    width: 130px;
-    border-radius: 6px;
+    width: 200px;
+    height: 35px;
+    border-radius: 3px;
+    text-align: justify;
+    background-color: rgb(236, 236, 234);
     border: solid 1px black;
 }
 h3{
     margin: 1ch;
+}
+
+#date{
+    display: flex;
+    align-items: center;
+
 }
 </style>
